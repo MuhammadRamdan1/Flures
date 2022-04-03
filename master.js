cbacbc1910d55c81601a696d050ff5e670b1c2a0
MohamedRamdan1 <medo44448888@gmail.com>
2022-04-03 03:48:46 +0200

    added animation

--- master.js
+++ master.js
@@ -18,3 +18,26 @@ contentBody1.addEventListener('click',function (){
 
 
 /* ــــــــــــــــــــــــــــــــــــــ[Footer]ــــــــــــــــــــــــــــــــــــــ */
+// const footer = document.querySelector(".semifooter");
+//     setTimeout( window.onload = function animation(){
+//         // footer.style.delay = '1s'
+//             footer.style.top = '0px'
+//         }
+//     , 2000);
+
+const footer = document.querySelector(".semifooter");
+window.onload = function(){
+    setTimeout(function(){
+        footer.style.top = '0px'
+    }, 1000);
+};
+
+// const footer = document.querySelector(".semifooter");
+// // window.onload =setTimeout(animation, 1000);
+// window.onload = function animation(){
+//     // footer.style.delay = '1s'
+//     footer.style.top = '0px'
+// }
+// footer.addEventListener('load',function(){
+//     footer.style.top = '0px'
+// })
